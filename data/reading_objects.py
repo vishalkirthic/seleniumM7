dd9e249222ad5f449fe24d92b05e134eb17ad67e
vishalkirthic <121278299+vishalkirthic@users.noreply.github.com>
2026-04-10 20:50:01 +0530

    Update reading_objects.py

--- data/reading_objects.py
+++ data/reading_objects.py
@@ -1,11 +1,16 @@
 import xlrd
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+file_path = os.path.join(BASE_DIR, "data", "locators.xlsx")
+
 #print(xlrd)
-path = r"C:\Users\Vishal Kirthic\PycharmProjects\framework_seleniumM7\data\locators.xlsx"
+# path = r"C:\Users\Vishal Kirthic\PycharmProjects\framework_seleniumM7\data\locators.xlsx"
 
 # xlrd.open_workbook(path)
 
 def read_locator(page_name):
-    workbook = xlrd.open_workbook(path)
+    workbook = xlrd.open_workbook(file_path)
 
     worksheet = workbook.sheet_by_name(page_name)
     rows = worksheet.get_rows()
